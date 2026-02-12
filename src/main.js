52223c8a163097ad0698f1bfd33e3029517196e6
Jonathan Ato Markin <jona10711@gmail.com>
2026-02-12 21:21:09 +0100

    feat: Disable developer tools, console logs, context menu, and specific keyboard shortcuts in production environments.

--- src/main.js
+++ src/main.js
@@ -32,4 +32,28 @@ app.use(ToastService);
 app.use(ConfirmationService);
 app.use(pinia);
 
+if (import.meta.env.PROD) {
+    // Disable console logs
+    console.log = function () {};
+    console.warn = function () {};
+    console.info = function () {};
+
+    // Disable Vue Devtools
+    app.config.devtools = false;
+
+    // Disable Context Menu
+    document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+    // Disable Keyboard Shortcuts
+    document.addEventListener('keydown', (e) => {
+        if (
+            e.code === 'F12' ||
+            (e.ctrlKey && e.shiftKey && ['KeyI', 'KeyJ', 'KeyC'].includes(e.code)) ||
+            (e.ctrlKey && e.code === 'KeyU')
+        ) {
+            e.preventDefault();
+        }
+    });
+}
+
 app.mount('#app');
